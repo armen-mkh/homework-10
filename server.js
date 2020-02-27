f90e362e3910bf872d81d82667c6ae97344a4353
Armen M <mkhitaryanarmen@yahoo.com>
2020-02-26 17:15:08 -0800

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs')
-const PORT = 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
